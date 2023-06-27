b90fc7b701c0150bd021b95f08b4a8d8aad16656
andertakecare <20363451+andertakecare@users.noreply.github.com>
2023-06-27 11:28:08 +0900

    create and import CSS modules

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from "./SearchBar.module.css";
 
 export function SearchBar() {
     return(
-        <>
+        <section className={styles.section}>
             <input type="text" placeholder="Enter a song title"></input>
+            <br />
             <button type="submit">Search</button>
-        </>
+        </section>
     )
 }
