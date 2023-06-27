1c0586caef270d5e3136fd58b09e535e619d10cd
andertakecare <20363451+andertakecare@users.noreply.github.com>
2023-06-27 11:28:08 +0900

    create and import CSS modules

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./Playlist.module.css";
 
 export function Playlist() {
     return (
-        <>
+        <section className={styles.section}>
             <h2>New Playlist</h2>
             <button type="submit">Save to Spotify</button>
-        </>
+        </section>
     )
 }
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Track } from "../Track/Track";
+import styles from "./SearchResults.module.css";
 
 export function SearchResults() {
   return (
-    <>
+    <section className={styles.section}>
       <h2>Results</h2>
       <Track />
-    </>
+    </section>
   );
 }
